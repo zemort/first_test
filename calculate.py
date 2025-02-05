3e441d69cc19b40b92b956a03515becba69a2abd
Aleksandr Efremov <zemort@yandex.ru>
2025-02-06 00:11:39 +0300

    Добавление функци

--- calculate.py
+++ calculate.py
@@ -4,6 +4,26 @@ division = '3. Деление'
 multiplication = '4. Умножение'
 exit_program = '0. Выход из программы'
 message_exit = ''
+def fold(num1, num2):
+    """"Сложение двух переменных"""
+    result = num1 + num2
+    return result
+
+def subtract(num1,num2):
+    """"Вычитание двух переменных"""
+    result = num1 - num2
+    return result
+
+def divide(num1, num2):
+    """"Деление двух переменных"""
+    result = num1 / num2
+    return result
+
+def multiply(num1,num2):
+    """"Умножение двух переменных"""
+    result = num1 * num2
+    return result
+
 while message_exit != exit_program:
     print("Пожалуйста, выберите операцию:",addition, subtraction, division, multiplication,exit_program, sep='\n')
     selected_operation = int(input())
@@ -15,18 +35,18 @@ while message_exit != exit_program:
         print('Введите второе число: ')
         second_number = float(input())
     if selected_operation == 1:
-        addition_finish = first_number + second_number
+        addition_finish = fold(first_number,second_number)
         print('Ответ:', addition_finish)
     if selected_operation == 2:
-        subtraction_finish = first_number - second_number
+        subtraction_finish = fold(first_number, second_number)
         print('Ответ:', subtraction_finish)
     if selected_operation == 3:
         while second_number == 0:
             print("Ошибка! Введите верное число!")
             second_number = float(input())
-        division_finish = first_number / second_number
+        division_finish = fold(first_number,second_number)
         print('Ответ:', division_finish)
     if selected_operation == 4:
-        multiplication_finish = first_number * second_number
+        multiplication_finish = fold(first_number, second_number)
         print('Ответ:', multiplication_finish)
 print('Завершение программы! Будем рады видеть Вас снова!')
