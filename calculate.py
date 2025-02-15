536bc307aefab2bf30a956804dcff0919b5ef9ce
xaxapetov <xaxapetov@yandex.ru>
2025-02-15 14:24:50 +0300

    Refactoring code

--- calculate.py
+++ calculate.py
@@ -1,8 +1,6 @@
 import datetime
-from functions import fold
-from functions import subtract
-from functions import divide
-from functions import multiply
+import functions
+
 addition = '1. Сложение'
 subtraction = '2. Вычитание'
 division = '3. Деление'
@@ -11,17 +9,12 @@ history = '5. История'
 exit_program = '0. Выход из программы'
 operation_history = []
 while True:
-    print("Пожалуйста, выберите операцию:",addition, subtraction, division, multiplication,history,exit_program, sep='\n')
+    print("Пожалуйста, выберите операцию:", addition, subtraction, division, multiplication, history, exit_program, sep='\n')
     selected_operation = int(input())
     if selected_operation == 0:
         break
     elif selected_operation == 5:
-        if not operation_history:
-            print("История пуста.")
-        else:
-            print("История операций: ")
-            for entry in operation_history:
-                print(entry)
+        functions.display_history(operation_history)
         continue
     else:
         print('Введите первое число: ')
@@ -30,24 +23,24 @@ while True:
         second_number = float(input())
     if selected_operation == 1:
         operation = "+"
-        result = fold(first_number,second_number)
+        result = functions.fold(first_number, second_number)
     elif selected_operation == 2:
         operation = "-"
-        result = subtract(first_number, second_number)
+        result = functions.subtract(first_number, second_number)
     elif selected_operation == 3:
         operation = "/"
         while second_number == 0:
             print("Ошибка! Введите верное число!")
             second_number = float(input())
-        result = divide(first_number,second_number)
+        result = functions.divide(first_number,second_number)
     elif selected_operation == 4:
         operation = "*"
-        result = multiply(first_number, second_number)
+        result = functions.multiply(first_number, second_number)
     else:
         print("Неверный выбор операции!")
         continue
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     history_entry = f"{timestamp} {first_number} {operation} {second_number} = {result}"
-    operation_history.append(history_entry)
+    operation_history.append(functions.create_history(first_number, operation, second_number, result))
     print(f"Ответ: {result}")
 print('Завершение программы! Будем рады видеть Вас снова!')
--- functions.py
+++ functions.py
@@ -1,19 +1,40 @@
+import datetime
+
 def fold(num1, num2):
     """"Сложение двух переменных"""
     result = num1 + num2
     return result
 
-def subtract(num1,num2):
+
+def subtract(num1, num2):
     """"Вычитание двух переменных"""
     result = num1 - num2
     return result
+
 
 def divide(num1, num2):
     """"Деление двух переменных"""
     result = num1 / num2
     return result
 
-def multiply(num1,num2):
+
+def multiply(num1, num2):
     """"Умножение двух переменных"""
     result = num1 * num2
     return result("operation_history")
+
+
+def create_history(num1, operation, num2, res):
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    history_entry = f"{timestamp} {num1} {operation} {num2} = {res}"
+    return history_entry
+
+
+def display_history(operation_history):
+    if not operation_history:
+        print("История пуста.")
+    else:
+        print("История операций: ")
+        for entry in operation_history:
+            print(entry)
